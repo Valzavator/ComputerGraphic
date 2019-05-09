f0790f359f415e750de9a8b55c95f5627467f422
Valzavator <maxsmv1998@gmail.com>
2019-05-09 04:01:36 +0300

    lab 6 done

--- Lab6/src/com/gmail/max/warrior/Warrior.java
+++ Lab6/src/com/gmail/max/warrior/Warrior.java
@@ -10,37 +10,56 @@ import javax.media.j3d.*;
 import javax.swing.*;
 import javax.vecmath.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-public class Warrior extends JFrame {
+class Warrior extends JFrame {
+    private Canvas3D canvas;
+    private SimpleUniverse universe;
 
-    //The canvas to be drawn upon
-    public Canvas3D myCanvas3D;
+    private TransformGroup headTG = new TransformGroup();
 
-    public Warrior() {
+    Warrior() {
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myCanvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+        configureWindow();
+        configureCanvas();
+        configureUniverse();
 
-        SimpleUniverse simpUniv = new SimpleUniverse(myCanvas3D);
-        simpUniv.getViewingPlatform().setNominalViewingTransform();
+        createSceneGraph();
+        addLight();
 
-        createSceneGraph(simpUniv);
-        addLight(simpUniv);
+        canvas.addKeyListener(new WarriorAnimation(headTG));
 
-        OrbitBehavior ob = new OrbitBehavior(myCanvas3D);
-        ob.setSchedulingBounds(new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE));
-        simpUniv.getViewingPlatform().setViewPlatformBehavior(ob);
-
-        setTitle("Warrior");
-        setSize(700, 700);
-        getContentPane().add(BorderLayout.CENTER, myCanvas3D);
         setVisible(true);
     }
 
+    private void configureWindow() {
+        setTitle("Warrior");
+        setSize(700, 700);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void configureCanvas() {
+        canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+        canvas.setDoubleBufferEnable(true);
+        canvas.setFocusable(true);
+        add(canvas, BorderLayout.CENTER);
+    }
+
+    private void configureUniverse() {
+        universe = new SimpleUniverse(canvas);
+        universe.getViewingPlatform().setNominalViewingTransform();
+
+        OrbitBehavior ob = new OrbitBehavior(canvas);
+        ob.setSchedulingBounds(new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE));
+        universe.getViewingPlatform().setViewPlatformBehavior(ob);
+    }
+
     @SuppressWarnings("Duplicates")
-    public void createSceneGraph(SimpleUniverse su) {
+    private void createSceneGraph() {
 
         ObjectFile f = new ObjectFile(ObjectFile.RESIZE);
         Scene warriorScene = null;
@@ -62,98 +81,104 @@ public class Warrior extends JFrame {
 
         TransformGroup sceneGroup = new TransformGroup();
 
-        Hashtable mikeNamedObjects = warriorScene.getNamedObjects();
+        Hashtable mikeNamedObjects = warriorScene != null
+                ? warriorScene.getNamedObjects()
+                : new Hashtable();
+
         Enumeration enumer = mikeNamedObjects.keys();
         while (enumer.hasMoreElements()) {
             String name = (String) enumer.nextElement();
             System.out.println("Name: " + name);
         }
 
+        Appearance bodyApp = new Appearance();
+        setDefaultAppearance(bodyApp, new Color3f(new Color(220, 207, 130)));
+
+        Appearance axeApp = new Appearance();
+        setDefaultAppearance(axeApp, new Color3f(new Color(86, 86, 86)));
+
         Shape3D head = (Shape3D) mikeNamedObjects.get("head");
+        head.setAppearance(bodyApp);
+
         Shape3D leftHand = (Shape3D) mikeNamedObjects.get("left_hand");
+        leftHand.setAppearance(bodyApp);
+
         Shape3D rightHand = (Shape3D) mikeNamedObjects.get("right_hand");
+        rightHand.setAppearance(bodyApp);
+
         Shape3D axe = (Shape3D) mikeNamedObjects.get("axe");
-        Shape3D body1 = (Shape3D) mikeNamedObjects.get("body1");
-        Shape3D body2 = (Shape3D) mikeNamedObjects.get("body2");
+        axe.setAppearance(axeApp);
+
+        Shape3D upperBody = (Shape3D) mikeNamedObjects.get("body2");
+        upperBody.setAppearance(bodyApp);
+
+        Shape3D lowerBody = (Shape3D) mikeNamedObjects.get("body1");
+        lowerBody.setAppearance(axeApp);
 
         TextureAttributes texAttr = new TextureAttributes();
         texAttr.setTextureMode(TextureAttributes.MODULATE);
 
-        TransformGroup bodyTG = new TransformGroup();
-        bodyTG.addChild(body1.cloneTree());
-        bodyTG.addChild(body2.cloneTree());
-
-
-        var headTG = new TransformGroup();
-        var leftHandTG = new TransformGroup();
-        var rightHandTG = new TransformGroup();
-        var axeTG = new TransformGroup();
+        TransformGroup leftHandTG = new TransformGroup();
+        TransformGroup rightHandTG = new TransformGroup();
+        TransformGroup axeTG = new TransformGroup();
+        TransformGroup upperBodyTG = new TransformGroup();
+        TransformGroup lowerBodyTG = new TransformGroup();
 
         headTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         leftHandTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         rightHandTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         axeTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        upperBodyTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
         headTG.addChild(head.cloneTree());
         leftHandTG.addChild(leftHand.cloneTree());
         rightHandTG.addChild(rightHand.cloneTree());
         axeTG.addChild(axe.cloneTree());
+        upperBodyTG.addChild(upperBody.cloneTree());
+        lowerBodyTG.addChild(lowerBody.cloneTree());
 
         BoundingSphere bounds = new BoundingSphere(new Point3d(0, 0, 0), Double.MAX_VALUE);
-//
-//        var tCrawl = new Transform3D();
-//        var tCrawl1 = new Transform3D();
-//        tCrawl.rotY(-90D);
-//        tCrawl1.rotX(-90D);
-//        long crawlTime = 10000;
-//        var crawlAlpha = new Alpha(1,
-//                Alpha.INCREASING_ENABLE,
-//                0,
-//                0, crawlTime, 0, 0, 0, 0, 0);
-//        float crawlDistance = 3.0f;
-//        var posICrawl = new PositionInterpolator(crawlAlpha,
-//                sceneGroup, tCrawl, -9.0f, crawlDistance);
-//
-//        long crawlTime1 = 30000;
-//        var crawlAlpha1 = new Alpha(1,
-//                Alpha.INCREASING_ENABLE,
-//                3000,
-//                0, crawlTime1, 0, 0, 0, 0, 0);
-//        float crawlDistance1 = 15.0f;
-//        var posICrawl1 = new PositionInterpolator(crawlAlpha1,
-//                sceneGroup, tCrawl1, -9.0f, crawlDistance1);
-//
-//
-//        var bs = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE);
-//        posICrawl.setSchedulingBounds(bs);
-//        posICrawl1.setSchedulingBounds(bs);
-//        sceneGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-//        sceneGroup.addChild(posICrawl1);
-
 
         int timeStart = 1000;
-        int timeRotationHour = 3000;
+        int timeRotationHour = 2000;
+
+        // HEAD
+
+        TransformGroup transformedHeadTG = new TransformGroup();
+        transformedHeadTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        transformedHeadTG.addChild(headTG);
 
         Transform3D headRotationAxis = new Transform3D();
         headRotationAxis.set(new Vector3d(-0.17, 0, -0.05));
-//        headRotationAxis.rotY(Math.PI/2);
-        headRotationAxis.setRotation(new AxisAngle4d(0, -0.1, 0, Math.PI/2));
-
+        headRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI / 2));
 
         Alpha headRotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE | Alpha.DECREASING_ENABLE, timeStart, 0,
                 timeRotationHour, 0, 0, timeRotationHour, 0, 0);
-        RotationInterpolator headRotation = new RotationInterpolator(headRotationAlpha, headTG,
-                headRotationAxis, (float) -Math.PI / 4 , (float) Math.PI / 4);
+        RotationInterpolator headRotation = new RotationInterpolator(headRotationAlpha, transformedHeadTG,
+                headRotationAxis, 0f, (float) Math.PI / 6);
         headRotation.setSchedulingBounds(bounds);
+
+        // UPPER BODY
+
+        Transform3D upperBodyRotationAxis = new Transform3D();
+        upperBodyRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI / 2));
+
+        Alpha upperBodyAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE | Alpha.DECREASING_ENABLE, timeStart, 0,
+                timeRotationHour, 0, 0, timeRotationHour, 0, 0);
+        RotationInterpolator upperBodyRotation = new RotationInterpolator(upperBodyAlpha, upperBodyTG,
+                upperBodyRotationAxis, 0f, (float) Math.PI / 6);
+        upperBodyRotation.setSchedulingBounds(bounds);
+
+        // LEFT HAND
 
         Transform3D leftHandTransform = new Transform3D();
 
         Transform3D leftHandXRot = new Transform3D();
-        leftHandXRot.setRotation(new AxisAngle4d(-0.1, 0, 0, 50* Math.PI / 180));
+        leftHandXRot.setRotation(new AxisAngle4d(-0.1, 0, 0, 50 * Math.PI / 180));
         leftHandTransform.mul(leftHandXRot, leftHandTransform);
 
         Transform3D leftHandYRot = new Transform3D();
-        leftHandYRot.setRotation(new AxisAngle4d(0, -0.1, 0, 50* Math.PI / 180));
+        leftHandYRot.setRotation(new AxisAngle4d(0, -0.1, 0, 50 * Math.PI / 180));
         leftHandTransform.mul(leftHandYRot, leftHandTransform);
 
         Transform3D leftHandTranslate = new Transform3D();
@@ -168,22 +193,24 @@ public class Warrior extends JFrame {
 
         Transform3D leftHandRotationAxis = new Transform3D();
         leftHandRotationAxis.set(new Vector3d(0, 0.23, -0.05));
-        leftHandRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI/2));
+        leftHandRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI / 2));
 
         Alpha leftHandRotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE | Alpha.DECREASING_ENABLE, timeStart, 0,
                 timeRotationHour, 0, 0, timeRotationHour, 0, 0);
         RotationInterpolator leftHandRotation = new RotationInterpolator(leftHandRotationAlpha, transformedLeftHandTG,
-                leftHandRotationAxis, (float)  -Math.PI , 0.0f);
+                leftHandRotationAxis, (float) -Math.PI / 2, (float) Math.PI / 4);
         leftHandRotation.setSchedulingBounds(bounds);
+
+        // RIGHT HAND
 
         Transform3D rightHandTransform = new Transform3D();
 
         Transform3D rightHandXRot = new Transform3D();
-        rightHandXRot.setRotation(new AxisAngle4d(-0.1, 0, 0, 50* Math.PI / 180));
+        rightHandXRot.setRotation(new AxisAngle4d(-0.1, 0, 0, 50 * Math.PI / 180));
         rightHandTransform.mul(rightHandXRot, rightHandTransform);
 
         Transform3D rightHandYRot = new Transform3D();
-        rightHandYRot.setRotation(new AxisAngle4d(0, -0.1, 0, -50* Math.PI / 180));
+        rightHandYRot.setRotation(new AxisAngle4d(0, -0.1, 0, -50 * Math.PI / 180));
         rightHandTransform.mul(rightHandYRot, rightHandTransform);
 
         Transform3D rightHandTranslate = new Transform3D();
@@ -198,27 +225,29 @@ public class Warrior extends JFrame {
 
         Transform3D rightHandRotationAxis = new Transform3D();
         rightHandRotationAxis.set(new Vector3d(0, 0.23, -0.05));
-        rightHandRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI/2));
+        rightHandRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI / 2));
 
         Alpha rightHandRotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE | Alpha.DECREASING_ENABLE, timeStart, 0,
                 timeRotationHour, 0, 0, timeRotationHour, 0, 0);
         RotationInterpolator rightHandRotation = new RotationInterpolator(rightHandRotationAlpha, transformedRightHandTG,
-                rightHandRotationAxis, (float)  -Math.PI , 0.0f);
+                rightHandRotationAxis, (float) -Math.PI / 2, (float) Math.PI / 4);
         rightHandRotation.setSchedulingBounds(bounds);
+
+        // AXE
 
         Transform3D axeTransform = new Transform3D();
 
-        Transform3D rotate1 = new Transform3D();
-        rotate1.setRotation(new AxisAngle4d(-0.1, 0, 0, 50* Math.PI / 180));
-        rightHandTransform.mul(rotate1, rightHandTransform);
+        Transform3D axeYRot = new Transform3D();
+        axeYRot.setRotation(new AxisAngle4d(0, -0.1, 0, Math.PI / 2));
+        axeTransform.mul(axeYRot, axeTransform);
 
-//        Transform3D rotate2 = new Transform3D();
-//        rotate2.setRotation(new AxisAngle4d(0, -0.1, 0, -50* Math.PI / 180));
-//        rightHandTransform.mul(rotate2, rightHandTransform);
-//
-//        Transform3D translate1 = new Transform3D();
-//        translate1.set(new Vector3d(0.02, 0.12, -0.12));
-//        rightHandTransform.mul(translate1, rightHandTransform);
+        Transform3D axeXRot = new Transform3D();
+        axeXRot.setRotation(new AxisAngle4d(-0.1, 0, 0, -Math.PI / 6));
+        axeTransform.mul(axeXRot, axeTransform);
+
+        Transform3D axeTranslate = new Transform3D();
+        axeTranslate.set(new Vector3d(-0.15, 0.5, 0.3));
+        axeTransform.mul(axeTranslate, axeTransform);
 
         axeTG.setTransform(axeTransform);
 
@@ -226,25 +255,28 @@ public class Warrior extends JFrame {
         transformedAxeTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         transformedAxeTG.addChild(axeTG);
 
-//        Transform3D rightHandRotationAxis = new Transform3D();
-//        rightHandRotationAxis.set(new Vector3d(0, 0.23, -0.05));
-//        rightHandRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI/2));
-//
-//        Alpha rightHandRotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE | Alpha.DECREASING_ENABLE, timeStart, 0,
-//                timeRotationHour, 0, 0, timeRotationHour, 0, 0);
-//        RotationInterpolator rightHandRotation = new RotationInterpolator(rightHandRotationAlpha, transformedRightHandTG,
-//                rightHandRotationAxis, (float)  -Math.PI , 0.0f);
-//        rightHandRotation.setSchedulingBounds(bounds);
+        Transform3D axeRotationAxis = new Transform3D();
+        axeRotationAxis.set(new Vector3d(0, 0.23, -0.05));
+        axeRotationAxis.setRotation(new AxisAngle4d(0, 0, -0.1, Math.PI / 2));
 
-        headTG.addChild(headRotation);
+        Alpha axeRotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE | Alpha.DECREASING_ENABLE, timeStart, 0,
+                timeRotationHour, 0, 0, timeRotationHour, 0, 0);
+        RotationInterpolator axeRotation = new RotationInterpolator(axeRotationAlpha, transformedAxeTG,
+                axeRotationAxis, (float) -Math.PI / 2, (float) Math.PI / 4);
+        axeRotation.setSchedulingBounds(bounds);
+
+        transformedHeadTG.addChild(headRotation);
         transformedLeftHandTG.addChild(leftHandRotation);
         transformedRightHandTG.addChild(rightHandRotation);
+        transformedAxeTG.addChild(axeRotation);
+        upperBodyTG.addChild(upperBodyRotation);
 
-        sceneGroup.addChild(headTG);
-        sceneGroup.addChild(bodyTG);
+        sceneGroup.addChild(transformedHeadTG);
         sceneGroup.addChild(transformedLeftHandTG);
         sceneGroup.addChild(transformedRightHandTG);
         sceneGroup.addChild(transformedAxeTG);
+        sceneGroup.addChild(upperBodyTG);
+        sceneGroup.addChild(lowerBodyTG);
 
         tgWarrior.addChild(sceneGroup);
 
@@ -258,20 +290,37 @@ public class Warrior extends JFrame {
         mainScene.addChild(bg);
         mainScene.compile();
 
-        su.addBranchGraph(mainScene);
+        universe.addBranchGraph(mainScene);
     }
 
-
-    private void addLight(SimpleUniverse simpleUniverse) {
+    private void addLight() {
         BranchGroup bgLight = new BranchGroup();
 
         BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
-        Color3f lightColour = new Color3f(1.0f, 1.0f, 1.0f);
-        Vector3f lightDir = new Vector3f(-1.0f, 0.0f, -0.5f);
-        DirectionalLight light = new DirectionalLight(lightColour, lightDir);
-        light.setInfluencingBounds(bounds);
 
-        bgLight.addChild(light);
-        simpleUniverse.addBranchGraph(bgLight);
+        Color3f lightColour = new Color3f(new Color(193, 193, 193));
+
+        DirectionalLight directionalLight1 = new DirectionalLight(lightColour, new Vector3f(-1.0f, 0, -0.5f));
+        directionalLight1.setInfluencingBounds(bounds);
+
+        DirectionalLight directionalLight2 = new DirectionalLight(lightColour, new Vector3f(0, -1.0f, -1.0f));
+        directionalLight2.setInfluencingBounds(bounds);
+
+        bgLight.addChild(directionalLight1);
+        bgLight.addChild(directionalLight2);
+
+        universe.addBranchGraph(bgLight);
+    }
+
+    private static void setDefaultAppearance(Appearance app, Color3f col) {
+        Material material = new Material();
+        material.setEmissiveColor(new Color3f(Color.BLACK));
+        material.setAmbientColor(col);
+        material.setDiffuseColor(col);
+        material.setSpecularColor(col);
+        material.setShininess(64);
+        material.setLightingEnable(true);
+
+        app.setMaterial(material);
     }
 }
